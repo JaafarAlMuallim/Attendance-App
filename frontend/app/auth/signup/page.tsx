8fcc2b8ffcf0d09e0b6d65399319fafab05bbc00
JaafarAlMuallim <Ja3far03@gmail.com>
2023-08-15 03:04:51 +0300

    frontend: added next-auth for authentication

---
+++ frontend/app/auth/signup/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import FormAR from "@/app/components/Form/Form-ar";
+import FormEN from "@/app/components/Form/Form-en";
+import { LangContext } from "@/store/lang-store";
+import { useContext } from "react";
+
+export default function SignUpForm() {
+  const langContext = useContext(LangContext);
+  return (
+    <div
+      dir={langContext.lang === "en" ? "ltr" : "rtl"}
+      className="flex justify-center my-20 bg-slate-500 bg-opacity-30 rounded-xl mx-5 md:rounded-2xl md:mx-96"
+    >
+      {langContext.lang === "en" ? (
+        <FormEN mode={"signup"} />
+      ) : (
+        <FormAR mode={"signup"} />
+      )}
+    </div>
+  );
+}
